ad37c88274ab6c750c13c0f597acf8ba1be5c296
吴康正 <wukangzheng991025@foxmail.com>
2018-03-24 19:01:09 +0800

    Debug 0.1

---
+++ StackStart/Stack.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define STACK_INIT_SIZE 100;
+#define STACK_INCRESEMENT 10;
+
+typedef int SElemType;
+
+typedef struct {
+	SElemType *base;
+	SElemType *top;
+	int StackSize;
+}SqStack;
