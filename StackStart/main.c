2a57319d890596b2e42cb1e1641245c6c5eafe23
吴康正 <wukangzheng991025@foxmail.com>
2018-03-31 10:14:59 +0800

    Debug 0.2

--- StackStart/main.c
+++ StackStart/main.c
@@ -3,5 +3,17 @@
 
 int main(void)
 {
-
+	LinkStack s = LinkStackInit();
+	SElemType input;
+	while (1)
+	{
+		input = getchar();
+		wipe_cache(stdin);
+		if (input == '$')
+			break;
+		Push(s, input);
+	}
+	StackPrint(s);
+	system("pause");
+	return 0;
 }
